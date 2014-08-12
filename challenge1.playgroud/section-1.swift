09f7faef0bd8467690acbaa28c1d13577bea67c5
Scott <slhodson969@gmail.com>
2014-08-12 19:33:41 +0200

    Challenge 1

---
+++ challenge1.playgroud/section-1.swift
@@ -0,0 +1,9 @@
+let preTaxBill = 19.99
+let tipPercentage = 0.20
+let taxPercentage = 0.06
+
+let tax = preTaxBill * taxPercentage
+let tip = preTaxBill * tipPercentage
+let total = preTaxBill + tax + tip
+
+
